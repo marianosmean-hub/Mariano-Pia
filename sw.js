9ed80451cf9d01e5b8b9a659b16c9b52e25054ed
marianosmean-hub <mariano.smean@gmail.com>
2025-08-27 19:28:09 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,8 +1,1 @@
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open('mariano-cache-v4').then(cache => cache.addAll([
-    './index.html','./app.webmanifest','./icon-192.png','./icon-512.png'
-  ])));
-});
-self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then(resp => resp || fetch(e.request)));
-});
+// Service Worker placeholder
